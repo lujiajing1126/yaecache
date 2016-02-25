2a1d1df80884fe7142e90a92137b767b8209da2c
Megrez <lujiajing1126@gmail.com>
2016-02-25 17:29:12 +0800

    add daemon & cmakedefine

--- yaecache.c
+++ yaecache.c
@@ -15,6 +15,18 @@ static void settings_init(void);
 /** exported globals **/
 struct settings settings;
 
+/** signal handler */
+#ifndef HAVE_SIGIGNORE
+static int sigignore(int sig) {
+    struct sigaction sa = { .sa_handler = SIG_IGN, .sa_flags = 0 };
+
+    if (sigemptyset(&sa.sa_mask) == -1 || sigaction(sig, &sa, 0) == -1) {
+        return -1;
+    }
+    return 0;
+}
+#endif
+
 static void signal_handler(const int sig) {
     printf("Signal handled: %s.\n", strsignal(sig));
     exit(EXIT_SUCCESS);
@@ -22,25 +34,67 @@ static void signal_handler(const int sig) {
 
 static void settings_init(void) {
 	settings.maxconns = 1024;
+	settings.verbose = 0;
+}
+
+static void usage(void) {
+	printf("show usage\n");
+}
+
+static void usage_licence(void) {
+	printf("show licence\n");
 }
 
 
 int main(int argc, char **argv) {
 	/* init local vars */
+	int ch;//return val of getopt
 	int maxcore = 0;
 	struct rlimit rlim;
 	char *username = NULL;
 	struct passwd *pw;
+	bool do_daemonize = false;
 
 	/* handle SIGINT and SIGTERM */
 	signal(SIGINT,signal_handler);
 	signal(SIGTERM,signal_handler);
 
 	settings_init();
+
 	/* set stderr non-buffering (for running under, say, daemontools) */
     setbuf(stderr, NULL);
 
     // process arguments
+    while(-1 != (ch = getopt(argc,argv,
+    	"u:"  /* user identity to run as */
+    	"hiV" /* help, licence info, version */
+    	"d"   /* daemon mode */
+    	"v"   /* verbose */
+    	))) {
+    	switch(ch) {
+    		case 'h':
+            	usage();
+            	exit(EXIT_SUCCESS);
+            case 'i':
+            	usage_licence();
+            	exit(EXIT_SUCCESS);
+            case 'v':
+            	settings.verbose++;
+            	break;
+            case 'V':
+            	printf(PACKAGE " " VERSION "\n");
+            	exit(EXIT_SUCCESS);
+    		case 'u':
+    			username = optarg;
+    			break;
+    		case 'd':
+            	do_daemonize = true;
+            	break;
+    		default:
+    			fprintf(stderr, "Illegal argument \"%c\"\n", ch);
+            	return 1;
+    	}
+    }
     
     // init limit resources
     if (maxcore != 0) {
@@ -67,8 +121,8 @@ int main(int argc, char **argv) {
     	rlim.rlim_max = settings.maxconns;
     	if(setrlimit(RLIMIT_NOFILE,&rlim) != 0) {
     		fprintf(stderr, "failed to set rlimit for open files. Try starting as root or requesting smaller maxconns value.\n");
+    		exit(EX_OSERR);
     	}
-    	exit(EX_OSERR);
     }
 
     // init users and groups things
@@ -83,10 +137,26 @@ int main(int argc, char **argv) {
     	}
 
     	if(setgid(pw->pw_gid) < 0 || setuid(pw->pw_uid) < 0) {
-    		 fprintf(stderr, "failed to assume identity of user %s\n", username);  
-        	exit(EX_OSERR); 
+    		fprintf(stderr, "failed to assume identity of user %s\n", username);
+        	exit(EX_OSERR);
     	}
     }
+
+    /* daemonize if requested */
+    /* if we want to ensure our ability to dump core, don't chdir to / */
+    if (do_daemonize) {
+        if (sigignore(SIGHUP) == -1) {
+            perror("Failed to ignore SIGHUP");
+        }
+        if (daemonize(maxcore, settings.verbose) == -1) {
+            fprintf(stderr, "failed to daemon() in order to daemonize\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    #if HAVE_STDBOOL_H
+    printf("use stdbool");
+    #endif
 
 	exit(EXIT_SUCCESS);
 }
--- yaecache.h
+++ yaecache.h
@@ -1,3 +1,18 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+/* When adding a setting, be sure to update process_stat_settings */
+/**
+ * Globally accessible settings as derived from the commandline.
+ */
 struct settings {
 	int maxconns;
+	int verbose;
 };
+
+/*
+ * Functions
+ */
+
+extern int daemonize(int nochdir, int noclose);
